51ea8556368aee6b3a326f67fab06005aa6b0e16
Ken <kwreyes620@gmail.com>
2018-11-29 23:53:32 +0800

    changed settings.py

--- django_first/settings.py
+++ django_first/settings.py
@@ -141,17 +141,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
 
 
 #MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
