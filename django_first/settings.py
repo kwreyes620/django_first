96ce7977076ffefb0dafad37c91bbaf192178f52
Ken <kwreyes620@gmail.com>
2018-11-30 00:27:17 +0800

    return root crispy login

--- django_first/settings.py
+++ django_first/settings.py
@@ -150,9 +150,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 
-#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-#MEDIA_URL = '/media/'
-#CRISPY_TEMPLATE_PACK = 'bootstrap4'
-#LOGIN_REDIRECT_URL = 'blog-home'
-#LOGIN_URL = 'login'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL = 'login'
 
