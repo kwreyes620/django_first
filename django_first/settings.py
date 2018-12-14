4d7b47b365e28604ab53fc98cbf07340725b03d4
Ken <kwreyes620@gmail.com>
2018-12-14 14:49:50 +0800

    static files

--- django_first/settings.py
+++ django_first/settings.py
@@ -150,6 +150,13 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 
+# collect extra static files 
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
